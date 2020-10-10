4f06d7accbf88a68ed1392b44bebdd66461cc282
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-10-10 23:52:16 +0530

    added product flow

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -22,22 +22,17 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
-
+    const { email, password } = req.body;
+    console.log(req.body);
     // Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
 
     // Store the id of that user inside the users cookie
     req.session.userId = user.id; // Added by cookie session!
 
-    res.send("Account created!!!");
+    res.redirect("/admin/products");
   }
 );
 
@@ -53,19 +48,14 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id; // Added by cookie session!
-    res.send("You are Signed In.");
+    res.redirect("/admin/products");
   }
 );
 
--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,21 +1,39 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
+const multer = require("multer");
+
+const { handleErrors, requireAuth } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const productsIndexTemplate = require("../../views/admin/products/index");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router(); // making router for effecting track of routes in other files as well
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", (req, res) => {});
+router.get("/admin/products", requireAuth, async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
+});
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  res.send("submitted");
-});
+router.post(
+  "/admin/products/new",
+  requireAuth,
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.redirect("/admin/products");
+  }
+);
 
 module.exports = router;
