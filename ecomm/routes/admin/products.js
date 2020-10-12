799d3d463305cca1bd8e4276ef9a164ecab77580
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-10-12 11:30:32 +0530

    project complete

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -5,6 +5,7 @@ const { handleErrors, requireAuth } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const productsIndexTemplate = require("../../views/admin/products/index");
+const productEditTemplate = require("../../views/admin/products/edit");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router(); // making router for effecting track of routes in other files as well
@@ -35,5 +36,46 @@ router.post(
     res.redirect("/admin/products");
   }
 );
+
+router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+
+  if (!product) {
+    return res.send("Product not found");
+  }
+
+  res.send(productEditTemplate({ product }));
+});
+
+router.post(
+  "/admin/products/:id/edit",
+  requireAuth,
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleErrors(productEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString("base64");
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send("could not find item");
+    }
+
+    res.redirect("/admin/products");
+  }
+);
+
+router.post("/admin/products/:id/delete", requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect("/admin/products");
+});
 
 module.exports = router;
